d9949ae115c5eb711d8ad62edd3dd25a5e1eea18
Challa <challasrinivas315@gmail.com>
2024-09-30 10:43:06 +0530

    Selenium_Project

---
+++ SeleniumProject/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,25 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+@Test
+public void createContactTest() {
+	String URL = System.getProperty("url","http://localhost:8888");
+	String BROWSER = System.getProperty("browser","firefox");
+	String USERNAME = System.getProperty("username","admin");
+	String PASSWORD = System.getProperty("password","admin");
+	System.out.println("execute createOrg test");
+	System.out.println(URL);
+	System.out.println(BROWSER);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
+	
+	
+	
+}
+@Test
+public void modifyContactTest() {
+	System.out.println("execute modifyOrg test");
+}
+}
